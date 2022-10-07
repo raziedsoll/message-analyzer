0692e01d45b90e1384b62ed7f6c3d1414fb3114f
Ramzi Al-Dorikhim <ramzik149@gmail.com>
2022-10-08 02:10:50 +0300

    Add hometask-2

---
+++ core/src/test/java/liga/medical/medicalanalyzing/core/MedicalAnalyzingApplicationTest.java
@@ -0,0 +1,11 @@
+package liga.medical.medicalanalyzing.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MedicalAnalyzingApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
